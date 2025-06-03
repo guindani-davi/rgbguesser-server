a10fcbf6df83c7d0cff7c6c8fc406f80aac61590
Davi-Guindani <davigpvieira@gmail.com>
2025-06-03 15:27:24 -0300

    refactor: move eslint config option languageOptions from one object to another

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,9 +12,6 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     plugins: { js },
     extends: ["js/recommended"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
     languageOptions: { globals: globals.node },
   },
   tseslint.configs.recommended,
